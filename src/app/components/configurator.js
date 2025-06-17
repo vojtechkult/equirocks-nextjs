5450f4d78f2807c8e7fcdfae3f350cc8ae871c3d
Vojtěch Kult <vojtechkult@expirio.cz>
2025-06-17 16:20:59 +0200

    Commit 3

---
+++ src/app/components/configurator.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+
+const Configurator = () => {
+    
+    return (
+        <div className="configurator" id="configurator">
+        </div>
+    );
+};
+
+export default Configurator;
