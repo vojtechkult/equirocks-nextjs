cf42f446b5185c8d9eb81b1c01ac6e93a9489aeb
Vojtěch Kult <vojtechkult@expirio.cz>
2025-06-24 14:36:31 +0200

    Design login stránky

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
+
+  const errorMessage = useRef(null);
 
   const handleLogin = async () => {
     const res = await fetch('/api/login', {
@@ -19,15 +21,25 @@ export default function LoginPage() {
       router.push('/protected');
     } else {
       console.error('Login error:', data);
-      alert(`Chyba: ${data.message}`);
+      message(data.message);
     }
   };
 
+  function message(message) {
+    errorMessage.current.style.display = "block";
+    errorMessage.current.textContent = message;
+  }
+
+
   return (
-    <div>
-      <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Heslo" />
-      <button onClick={handleLogin}>Přihlásit se</button>
+    <div className="page-wrapper">
+      <div className="login-wrapper">
+        <h2>EquiRocks konfigurátor</h2>
+        <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Heslo" />
+        <p ref={errorMessage}>ERROR</p>
+        <button onClick={handleLogin}>Přihlásit se</button>
+      </div>
     </div>
   );
 }
